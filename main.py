d39b35d8f4dad8c647a4c11b2900ed0a030759a4
SosiskaSunrise <raketa3322@mail.ru>
2025-05-18 09:13:35 -0700

    Init commit

--- main.py
+++ main.py
@@ -1,2 +1,80 @@
-import arcade
+import random
 
+import arcade
+from pyglet.event import EVENT_HANDLE_STATE
+
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "PING PONG TABLE"
+SPEED_X = 5
+SPEED_Y = -7
+SPEED_BAR = 6.4
+
+
+class Ball(arcade.Sprite):
+    def update(self, delta_time: float = 1 / 60, *args, **kwargs) -> None:
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+        if self.right > SCREEN_WIDTH or self.left < 0:
+            self.change_x = -self.change_x
+        if self.top > SCREEN_HEIGHT or self.bottom < 0:
+            self.change_y = -self.change_y
+
+
+class Bar(arcade.Sprite):
+    def update(self, delta_time: float = 1 / 60, *args, **kwargs) -> None:
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+
+class Game(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width=width, height=height, title=title)
+
+        self.ball = Ball("ball.png", 0.1)
+
+        self.bar = Bar("bar.png", 0.1)
+        self.setup()
+
+    def setup(self):
+        self.ball.center_x = SCREEN_WIDTH / 2
+        self.ball.center_y = SCREEN_HEIGHT / 2
+        self.ball.change_x = SPEED_X
+        self.ball.change_y = SPEED_Y
+
+        self.bar.center_x = SCREEN_WIDTH / 2
+        self.bar.center_y = SCREEN_HEIGHT * 4 / 100
+        direction = random.randint(1, 2)
+        if direction < 2:
+            self.ball.change_x = -SPEED_X
+        else:
+            self.ball.change_x = SPEED_X
+            self.ball.change_y = SPEED_Y
+            self.bar.center_x = SCREEN_WIDTH / 2
+
+    def on_update(self, delta_time: float) -> bool | None:
+        self.ball.update()
+        self.bar.update()
+        if arcade.check_for_collision(self.bar, self.ball):
+            self.ball.bottom = self.bar.top + 1
+            self.ball.change_y = -self.ball.change_y
+
+    def on_draw(self):
+        self.clear((100, 149, 237))
+        arcade.draw_sprite(self.ball)
+        arcade.draw_sprite(self.bar)
+
+    def on_key_press(self, symbol: int, modifiers: int) -> EVENT_HANDLE_STATE:
+        if symbol == arcade.key.LEFT:
+            self.bar.change_x = -SPEED_BAR
+        if symbol == arcade.key.RIGHT:
+            self.bar.change_x = SPEED_BAR
+
+    def on_key_release(self, symbol: int, modifiers: int) -> EVENT_HANDLE_STATE:
+        if symbol == arcade.key.LEFT or symbol == arcade.key.RIGHT:
+            self.bar.change_x = 0
+
+
+game = Game(width=SCREEN_WIDTH, height=SCREEN_HEIGHT, title=SCREEN_TITLE)
+
+arcade.run()
